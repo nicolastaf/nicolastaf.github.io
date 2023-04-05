23ac2a1bd2439072441f22090a566c6450ba09d9
nicolastaf <nicolas.staffent@gmail.com>
2023-04-05 18:08:57 +0200

    delete line base

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://nicolastaf.github.io',
-    base: '/nicolastaf.github.io',
+    //base: '/nicolastaf.github.io',
 });
